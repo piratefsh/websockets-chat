1572fed3ebd5d4e5da381f7b155edc407905b535
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-30 10:05:27 +0800

    Make webapp a regular dir

---
+++ templates/app/js/index.js
@@ -0,0 +1,84 @@
+var app = angular.module('chat', []);
+
+app.controller('ChatController', ['$scope',
+    function($scope) {
+        $scope.messages = [];
+        $scope.alerts = [];
+        $scope.username = "";
+        $scope.input = ""
+        $scope.status = {
+            connected: false,
+            error: false,
+            errorMsg: 'off'
+        }
+
+        $scope.init = function() {
+            // $scope.socket = io.connect('http://websockets-chat-dev.elasticbeanstalk.com/echo');
+            $scope.socket = io.connect('http://' + 'localhost' + ':' + '5000' + '/echo');
+
+            $scope.socket.emit('join', {
+                data: $scope.username
+            });
+            $scope.socket.on('joined', function(e) {
+                $scope.$apply(function() {
+                    // $scope.alerts.push(e.data)
+                });
+            })
+            $scope.socket.on('connect', function(e) {
+                $scope.$apply(function() {
+                    $scope.status.connected = true;
+                    $scope.status.error = false;;
+                    // $scope.alerts.push('Connected')
+                    // $scope.alerts.push(e.users.length + ' users online: ' + e.users.join(', '))
+                });
+            });
+
+            $scope.socket.on('disconnect', function(e) {
+                $scope.$apply(function() {
+                    $scope.status.connected = false;
+                    $scope.status.error = true;
+                    $scope.status.errorMsg = "disconnected";
+                });
+            });
+
+            $scope.socket.on('response', function(e) {
+                $scope.$apply(function() {
+                    if (e.data.user != $scope.username){
+                      $scope.messages.push({
+                        isMe: false,
+                        user: e.data.user,
+                        text: e.data.text
+                      });
+                    }
+                });
+            });
+        }
+
+        $scope.sendMessage = function() {
+            if ($scope.status.connected && $scope.input) {
+                $scope.messages.push({
+                    isMe: true,
+                    text: $scope.input,
+                });
+
+                $scope.socket.emit('message', {
+                    data: {
+                      text: $scope.input,
+                      user: $scope.username
+                    }
+                });
+                $scope.input = "";
+            }
+        }
+
+        $scope.login = function(){
+          if($scope.username){
+            $scope.init();
+          }
+          else{
+            $scope.alerts.push("Please enter a username")
+          }
+        }
+
+    }
+]);
